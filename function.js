e646528e06329334d7876a683dffb7ac04c574ab
Shokanta Roy <shokantaroy.2002@gmail.com>
2026-07-25 18:06:49 +0600

    function

--- function.js
+++ function.js
@@ -1,16 +1,36 @@
 //Function Declaration
-function functionname(parameters) {
-    //code
-}
+// function functionname(parameters) {
+//     //code
+// }
 
-function add(a,b) {
-    return a+b;
-}
-console.log(add(10,20));
+// function add(a,b) {
+//     return a+b;
+// }
+// console.log(add(10,20));
 
 //Function Declaration Hoisting
 hello();
 
 function hello() {
-    console.log("Hello javaScript");
+    console.log("Hello, roy");
 }
+//Function expression 
+const variableName = function(perameters) {
+    //code
+}
+// const add = function(a,b) {
+//     return a+b;
+// }
+// console.log(add(5,7));
+
+
+//arrow function
+const add = (a,b) => {
+    return a+b;
+}
+console.log(add(50,60));
+
+const multi = (a,b) => {
+    return a*b;
+}
+console.log(multi(4,5));
