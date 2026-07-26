79684c54bcdadb1f00b2be241b13d81edc3b994f
Shokanta Roy <shokantaroy.2002@gmail.com>
2026-07-27 05:41:28 +0600

    string method

---
+++ string_method.js
@@ -0,0 +1,22 @@
+//split()--- convert string to array
+//split a string into parts based on a separator
+// const text = "apple,banana,mango";
+// const result = text.split(",");
+// console.log(result);
+// const word = "Shokanta";
+// console.log(word.split(""));
+
+//join()
+//cmpletely oposite of split()
+// joins array elements into a string
+const fruits = ["apple", "banana", "mango"];
+const result = fruits.join("-");
+console.log(result);
+
+
+
+//includes()---------- check existence
+//return true and false value
+//case sensitive
+const text = "I love JavaScript";
+console.log(text.includes("Love"));
