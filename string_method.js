923def587ac169cee0fd3d96e4db0fb5005eddbd
Shokanta Roy <shokantaroy.2002@gmail.com>
2026-07-28 01:58:31 +0600

    updated string method

--- string_method.js
+++ string_method.js
@@ -9,14 +9,47 @@
 //join()
 //cmpletely oposite of split()
 // joins array elements into a string
-const fruits = ["apple", "banana", "mango"];
-const result = fruits.join("-");
-console.log(result);
-
-
+// const fruits = ["apple", "banana", "mango"];
+// const result = fruits.join("-");
+// console.log(result);
 
 //includes()---------- check existence
 //return true and false value
 //case sensitive
-const text = "I love JavaScript";
-console.log(text.includes("Love"));
+// const text = "I love JavaScript";
+// console.log(text.includes("Love"));
+
+// //slice()--- extract part of string
+// //Does not change the original string
+// //extract a portion from a string
+// // String.slice(start, end);
+// const name = "Shokanta Roy";
+// console.log(name.slice(0, 5));
+
+// //replace()--- Replace text
+// const sentence = "I love my career";
+// const result = sentence.replace("career", "family");
+// console.log(result);
+
+
+
+
+//Real Example
+const sentence = "I love coding";
+//split
+const words = sentence.split(" ");
+//join
+const newSentence = words.join("-");
+//includes
+const hasLove = sentence.includes("love");
+//slice
+const part = sentence.slice(2,6);
+//replce
+const updated = sentence.replace("coding", "nodeJs");
+
+console.log(sentence);
+console.log(words);
+console.log(newSentence);
+console.log(hasLove);
+console.log(part);
+console.log(updated);
