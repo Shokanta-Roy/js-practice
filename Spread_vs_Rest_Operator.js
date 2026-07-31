5ec59ea077597891a18580551f2675625708220d
Shokanta Roy <shokantaroy.2002@gmail.com>
2026-07-31 23:48:43 +0600

    new

--- Spread_vs_Rest_Operator.js
+++ Spread_vs_Rest_Operator.js
@@ -1,5 +1,16 @@
 //Spread operator
+//think like, "Open or Expand"
+
 const arr1 = [1, 2];
 const arr2 = [...arr1, 3, 4];
 
 console.log(arr2);
+
+const user = { name: "John", age: 25 };
+
+const newUser = {
+  ...user,
+  country: "BD",
+};
+
+console.log(newUser);
