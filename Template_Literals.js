73fa5e4d17c3af58ae02dc5ecfc5a91c5e0ca9aa
Shokanta Roy <shokantaroy.2002@gmail.com>
2026-08-02 03:15:51 +0600

    backtics

---
+++ Template_Literals.js
@@ -0,0 +1,13 @@
+//use backtics instend of using double qoute
+let num1 = 10;
+let num2 =20;
+
+let result = num1 + num2;
+
+console.log(result);
+console.log("addition of num1 and num2 is result");
+console.log("addition of " + num1 + " and " + num2 + " is " + result);
+console.log(`addition of ${num1} and ${num2} is ${result}`);
+console.log(`hi, I am shokanta roy.
+    I am from UIU`);
+console.log(`sum is ${num1+num2}`);
