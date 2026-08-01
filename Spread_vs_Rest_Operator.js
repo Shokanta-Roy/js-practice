45f8d5b2ff4320154afd06f2252033eaeabe5bb5
Shokanta Roy <shokantaroy.2002@gmail.com>
2026-08-01 07:30:43 +0600

    spread

--- Spread_vs_Rest_Operator.js
+++ Spread_vs_Rest_Operator.js
@@ -1,16 +1,37 @@
 //Spread operator
 //think like, "Open or Expand"
+//spread is used to expand an array into seperate elements.
 
-const arr1 = [1, 2];
-const arr2 = [...arr1, 3, 4];
+let numbers = [1, 2, 3, 4, 5];
+let maximum = Math.max(...numbers);
+let minimum = Math.min(...numbers);
 
-console.log(arr2);
+console.log(numbers);
+console.log(maximum);
+console.log(minimum);
 
-const user = { name: "John", age: 25 };
 
-const newUser = {
-  ...user,
-  country: "BD",
-};
+let userName = "shokanta";
+let letters = [...userName].join("-");
+console.log(letters);
 
-console.log(newUser);
+
+
+
+
+// const arr1 = [1, 2];
+// const arr2 = [...arr1, 3, 4];
+
+// console.log(arr2);
+
+// const user = { name: "John", age: 25 };
+
+// const newUser = {
+//   ...user,
+//   country: "BD",
+// };
+
+// console.log(newUser);
+
+
+//rest = bundles seperate elements into an array
